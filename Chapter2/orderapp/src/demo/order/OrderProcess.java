372d167b0308b9673d4dfcf64aea1ddf07295369
Piotr Piotrowski <piotr@piotrovski.com>
2014-12-11 23:26:34 +0100

    changes

--- Chapter2/orderapp/src/demo/order/OrderProcess.java
+++ Chapter2/orderapp/src/demo/order/OrderProcess.java
@@ -1,9 +1,15 @@
 package demo.order;
 
+import org.apache.cxf.service.model.ServiceInfo;
+
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
 public interface OrderProcess {
+
+    @WebMethod
     String processOrder(Order order);
+
 }
 
